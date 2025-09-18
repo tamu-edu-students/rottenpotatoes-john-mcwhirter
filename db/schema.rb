17e249c30051ad7252a2db5a1ea26dd364e24b06
Brea McWhirter <78233755+jmcwhirter1608@users.noreply.github.com>
2025-09-18 09:29:34 -0500

    Completing Part 3
    Created CRUD routes by adding the /movies route to routes.rb
    Created actions and views automatically with a generate command

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_18_135311) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_18_135618) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
